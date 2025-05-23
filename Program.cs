3f5f6a66d514ca835004583d01f1bf17d2a91909
asp <asp@LAB12-12>
2025-05-23 17:49:19 +0500

    arrays

---
+++ Program.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace eustus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Hello WOrld!");
+            //Console.Write("Hello WOrld! \n by Umar");
+
+            //Console.Write("Enter Your Name : ");
+            //string uname = Console.ReadLine();
+            //Console.Write("Enter Your email : ");
+            //string email = Console.ReadLine();
+            //Console.Write   ("Enter Your  father : ");
+            //string father = Console.ReadLine();
+            //Console.Write("Enter Your mother : ");
+            //string mother = Console.ReadLine();
+            //Console.Write("Enter Your  qualification : ");
+            //string qualification = Console.ReadLine();
+            //Console.Write("Enter Your  nationality : ");
+            //string nationality = Console.ReadLine();
+            //Console.Write("Enter Your  CNIC : ");
+            //string CNIC =  Console.ReadLine();
+            //Console.Write("Enter Your  address : ");
+            //string address = Console.ReadLine();
+
+            //Console.Write("Enter Your  age : ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Your  No OF Siblings : ");
+            //int no_of_siblings = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Your  Body mass index : ");
+            //int bmi = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Your  Martial Status : ");
+            //bool martial_status = bool.Parse(Console.ReadLine());
+            //Console.Write("Enter Your  have Children : ");
+            //bool have_children = bool.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine("User name is " + uname);
+            //Console.WriteLine("User father name is " + father);
+            //Console.WriteLine("User mother name is " + email);
+            //Console.WriteLine("User qualification is " + qualification);
+            //Console.WriteLine("User nationality is " + nationality);
+            //Console.WriteLine("User CNIC is " + CNIC);
+            //Console.WriteLine("User address is " + address);
+            //Console.WriteLine("User qualification is " + qualification);
+
+
+            //Console.WriteLine("User age is " + age);
+            //Console.WriteLine("User no of siblings is " + no_of_siblings);
+            //Console.WriteLine("User bmi is " + bmi);
+            //Console.WriteLine("User status of Martial is " + martial_status);
+            //Console.WriteLine("User have children" + martial_status);
+
+
+            //Console.Write("Employee Name : ");
+            //string e_name = Console.ReadLine();
+            //Console.Write("Employee Desgnation : ");
+            //string e_designantion = Console.ReadLine();
+            //Console.Write("Employee Department : ");
+            //string e_dep = Console.ReadLine();
+            //Console.Write("Employee Salary : ");
+            //int e_salary = int.Parse(Console.ReadLine());
+            //Console.Write("Workingt Days : ");
+            //int w_days = int.Parse(Console.ReadLine());
+            //Console.Write("Leaves : ");
+            //int leaves = int.Parse(Console.ReadLine());
+            //Console.Write("Is Permenant : ");
+            //bool is_permenant = bool.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Employee name is " + e_name);
+            //Console.WriteLine("Employee designation is " + e_designantion);
+            //Console.WriteLine("Employee department is " + e_dep);
+            //Console.WriteLine("Employee Salary is " + e_salary);
+            //Console.WriteLine("Employee working days is " + w_days);
+            //Console.WriteLine("Employee leaves " + leaves);
+            //Console.WriteLine("Employee is Permenant" + is_permenant);
+
+
+            //Console.Write("Enter Your unit usage : ");
+            //float u_usage = float.Parse(Console.ReadLine());
+
+            //float unit_per = 34.89f;
+
+            //float total = u_usage * unit_per;
+
+            //Console.WriteLine("Your Electricity Bill is " + total + " Ruppees");
+
+            //Console.Write("Enter Your Math Marks :"); 
+            //float Math_Marks = float.Parse(Console.ReadLine());
+            //Console.Write("Enter Your Urdu Marks :");
+            //float Urdu_Marks = float.Parse(Console.ReadLine());
+            //Console.Write("Enter Your Islamiat Marks :");
+            //float Islamiat_Marks = float.Parse(Console.ReadLine());
+            //Console.Write("Enter Your Englsih Marks :");
+            //float Englsih_Marks = float.Parse(Console.ReadLine());
+
+            //float total = Math_Marks + Urdu_Marks + Islamiat_Marks + Englsih_Marks;
+            //float average = total / 4;
+
+            //Console.WriteLine("Your Average Marks " + average);
+
+
+            //Console.Write("Enter Your  age : ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //string msg = age > 18 ? "you are egligible for vote" : "you are not egligible for vote";
+            //Console.Write(msg);
+
+            //ternery operators
+
+            //Console.Write("Enter number : ");
+            //float num1 = float.Parse(Console.ReadLine());
+
+            //Console.Write("Enter number : ");
+            //float num2 = float.Parse(Console.ReadLine());
+
+            //Console.Write("Do you want to add this number : ");
+            //string choice = (Console.ReadLine());
+
+
+
+            //float print_ans  = choice.ToLower() == "yes" ? num1 + num2 : num1 - num2;
+            //Console.WriteLine(print_ans);
+
+            //Console.Write("Enter amount in Pakistani Ruppes: ");
+            //float num1 = float.Parse(Console.ReadLine());
+
+            //Console.Write("Do you want to converted into dollars : ");
+            //string num2 = (Console.ReadLine());
+
+            //float print_ans = num2.ToLower() == "yes" ? num1 / 280 : 0 ;
+            //Console.WriteLine(print_ans);
+
+            //Console.Write("Enter First Number : ");
+            //float num1 = float.Parse(Console.ReadLine());
+
+            //Console.Write("Enter Second Number : ");
+            //float num2 = float.Parse(Console.ReadLine());
+
+            //Console.Write("Enter Opertoion To Perform : ");
+            //string operators = Console.ReadLine();
+            //float result = 0;
+
+            //if(operators == "+")
+            //{
+            //    result = num1 + num2; 
+            //}
+            //else if (operators == "-")
+            //{
+            //    result = num1 - num2;
+            //}
+
+            //else if (operators == "*")
+            //{
+            //    result = num1 * num2;
+            //}
+            //else if (operators == "/")
+            //{
+            //    result = num1 / num2;
+            //}
+            //else if (operators == "%")
+            //{
+            //    result = num1 % num2;
+            //}
+            //else
+            //{
+            //    result = 0;
+            //}
+
+            //switch (operators)
+            //{
+            //    case "+":
+            //        result = num1 + num2;
+            //        break;
+
+            //    case "-":
+            //        result = num1 - num2;
+            //        break;
+
+            //    case "*":
+            //        result = num1 * num2;
+            //        break;
+
+            //    case "/":
+            //        result = num1 / num2;
+            //        break;
+
+            //    case "%":
+            //        result = num1 % num2;
+            //        break;
+
+            //    default:
+            //        result = 0;
+            //        break;
+            //}
+
+
+
+            //Console.WriteLine(num1 + " " + operators + " " + num2 + " = " + result);
+
+
+            //Console.WriteLine(" -------------- Welcome To Bank ---------------");
+            //Console.Write("Enter Your Pin");
+            //int pass = int.Parse(Console.ReadLine());
+            //float balance = 1000f;
+
+            //if ( pass == 12345)
+            //{
+            //    Console.WriteLine("---------- Please select Menu ----------");
+            //    Console.Write("1- Check Balance \n 2- Cash Deposit \n 3- Cash WithDraw");
+            //    int opt = int.Parse(Console.ReadLine());
+            //    switch (opt)
+            //    {
+            //        case 1:
+            //            Console.Write("Your Balance Is : " + balance);
+            //            break;
+            //        case 2:
+            //            Console.WriteLine(" Deposit Into Your Account");
+            //            Console.Write("Enter Your Amount : ");
+            //            float depo_cash = float.Parse(Console.ReadLine());
+            //            float Total = depo_cash += balance;
+            //            break;
+
+
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Password");
+            //}
+
+            //for lopp,while,foreach,dowhile loop
+
+            //for (int count = 1; count <= 10; count++)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+            //for (int count = 0; count <= 100; count = count + 2)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+            //for (int count2 = 0; count2 <= 100; count2++)
+            //{
+            //    if (count2 % 2 != 0) {
+            //        Console.WriteLine(count2);
+            //    }
+            //}
+
+            //int sum = 0;
+
+            //for (int count3 = 0; count3 <= 100; count3++)
+            //{
+            //    Console.WriteLine(count3);
+            //    sum += count3;
+
+            //}
+            //Console.WriteLine("sum is" + sum);
+            //Console.ReadKey();
+            //    Console.Write("Plese Enter A number You want to Create a table :");
+            //    int table = int.Parse(Console.ReadLine());
+            //    int ans = 0, i = 1;
+
+            //    while (i <= 10)
+            //    {
+            //        ans = table * i;
+            //        Console.WriteLine(table + " x " + i + " = " + ans);
+            //        i++;
+            //    }
+
+            // do while loops
+
+            //string choice = "";
+            //float f_num, s_num, result = 0;
+            //do
+            //{
+            //    Console.Write(" Enter First Number : ");
+            //    f_num = float.Parse(Console.ReadLine());
+
+            //    Console.Write(" Enter Secind Number : ");
+            //    s_num = float.Parse(Console.ReadLine());
+
+            //    float result = f_num + s_num;
+            //    Console.WriteLine(f_num + " + " + s_num + " = " + result);
+
+            //    Console.Write(" do you wnat to Continue :");
+            //    choice = Console.ReadLine();
+
+
+            //}
+            //while (choice.ToLower() == "yes");
+
+
+            //foreach loop
+
+            string[] strings = { "Jupiter", "Mars", "Sun", " moon", "pluto", "starun" };
+
+
+            foreach ( var p in strings) {
+
+                Console.WriteLine(p);
+            }
+
+
+            
+        }
+    }
+    }
